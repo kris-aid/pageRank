88dd1b2a27097fdfcef5af96ea70c57e143266aa
Juanra300o2 <157748083+Juanra300o2@users.noreply.github.com>
2024-02-05 22:56:08 -0500

    Update pagerank.py

--- pagerank.py
+++ pagerank.py
@@ -21,40 +21,42 @@ def weighted_adjacency_matrix(graph):
 def prob_next_nodes(graph, current_node, pagerank_vector, teleport):
     size = len(graph.nodes)
     next_nodes_vector = np.zeros(size)
-    next_nodes= graph.out_edges(current_node)
+    next_nodes = graph.out_edges(current_node)
+    
     for node in next_nodes:
-        next_nodes_vector[node[1]-1] = 1
-    prob_next_nodes = []
+        next_nodes_vector[node[1] - 1] = 1
+    
     if teleport:
-        prob_next_nodes = [pagerank_vector[i] if next_nodes_vector[i] != 0 else 0 for i in range(len(pagerank_vector))]
-
-        norm=np.sum(prob_next_nodes)
-        if norm!=0:
-            return prob_next_nodes/norm
-        else:
-            return prob_next_nodes
+       prob_next_nodes = np.array([pagerank_vector[i] if next_nodes_vector[i] != 0 else 0 for i in range(len(pagerank_vector))]) * 0.8 + (np.ones(len(pagerank_vector)) / len(pagerank_vector)) * 0.2 
     else:
-        prob_next_nodes = [pagerank_vector[i] if next_nodes_vector[i] != 0 else 0 for i in range(len(pagerank_vector))]
-        norm=np.sum(prob_next_nodes)
-        if norm!=0:
-            return prob_next_nodes/norm
-        else:
-            return prob_next_nodes
+        prob_next_nodes = np.array([pagerank_vector[i] if next_nodes_vector[i] != 0 else 0 for i in range(len(pagerank_vector))])
+        
+    
+    norm = np.sum(prob_next_nodes)
+    
+    if norm != 0:
+        return prob_next_nodes / norm
+    else:
+        return prob_next_nodes
     
     
-def random_walk(graph, start_node,pagerank_vector, teleport):
+def random_walk(graph, start_node,pagerank_vector, teleport,walked_nodes):
     
     walk_prob=prob_next_nodes(graph, start_node, pagerank_vector, teleport)
+    for node in walked_nodes:
+        walk_prob[node - 1] = 0
     print("test prob",walk_prob)
     if any(weight != 0 for weight in walk_prob):
         current_node = random.choices(range(len(walk_prob)), weights=walk_prob)[0]
-        return current_node + 1 
+        current_node= current_node+1
+        
+        return current_node
     else:
         return start_node
 
 def calculate_pagerank_vector(graph, pagerank_vector, teleport):
     alpha=0.8
-    new_pagerank_vector=pagerank_vector
+    new_pagerank_vector=pagerank_vector.copy()
     M = weighted_adjacency_matrix(graph)
     n = len(graph.nodes)
     if teleport:      
@@ -63,16 +65,17 @@ def calculate_pagerank_vector(graph, pagerank_vector, teleport):
         new_pagerank_vector = M @ pagerank_vector
     else:
         new_pagerank_vector = M @ pagerank_vector
-
+    if np.linalg.norm(new_pagerank_vector - pagerank_vector, 1) < 1e-6:
+        print("convergence achieved")
     return new_pagerank_vector
 
-def power_iterate(graph, pagerank_vector, teleport, start_node,num_iterations):
+def power_iterate(graph, pagerank_vector, teleport, start_node,num_iterations,all_walked_nodes):
     pagerank_vectors=[pagerank_vector]
     walked_nodes=[start_node]
     walk_rate_nodes=[prob_next_nodes(graph, start_node,pagerank_vector,teleport)]
     for _ in range(num_iterations):
         pagerank_vectors.append(calculate_pagerank_vector(graph, pagerank_vectors[-1], teleport))
-        current_node=random_walk(graph,walked_nodes[-1],pagerank_vectors[-1],teleport)
+        current_node=random_walk(graph,walked_nodes[-1],pagerank_vectors[-1],teleport,all_walked_nodes)
         walked_nodes.append(current_node)
         walk_rate_nodes.append(prob_next_nodes(graph, walked_nodes[-1],pagerank_vectors[-1],teleport))
     return pagerank_vectors,walked_nodes, walk_rate_nodes,current_node
